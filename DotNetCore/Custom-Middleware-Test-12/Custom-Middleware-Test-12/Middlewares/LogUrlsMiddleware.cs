2cae4657636e45771be585750be4121d08faa1cb
Bajirao Shinde <bshinde@hsdyn.com>
2024-10-12 21:12:48 +0530

    Custom-Middleware-12Oct2024

---
+++ DotNetCore/Custom-Middleware-Test-12/Custom-Middleware-Test-12/Middlewares/LogUrlsMiddleware.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using System.Threading.Tasks;
+
+namespace Custom_Middleware_Test_12.Middlewares
+{
+    // You may need to install the Microsoft.AspNetCore.Http.Abstractions package into your project
+    public class LogUrlsMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public LogUrlsMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public Task Invoke(HttpContext httpContext)
+        {
+            Console.WriteLine(string.Format("Processing request{0}", httpContext.Request.Host + httpContext.Request.Path));
+            return _next(httpContext);
+        }
+    }
+
+    // Extension method used to add the middleware to the HTTP request pipeline.
+    public static class LogUrlsMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseLogUrlsMiddleware(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<LogUrlsMiddleware>();
+        }
+    }
+}
